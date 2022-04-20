802e6fe6f10c6249b8267c069e274a5e0d834c8a
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2022-04-20 05:50:30 +0000

    count notifications

--- resources/views/livewire/bull.blade.php
+++ resources/views/livewire/bull.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <span class="badge rounded-pill bg-danger badge-up" wire:poll >{{$NotificationsNumber}}</span>
+<div wire:poll>
+    @if($NotificationsNumber != 0)
+        <span class="badge rounded-pill bg-danger badge-up" >{{$NotificationsNumber}}</span>
+    @endif
 </div>
