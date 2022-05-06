4c709583bb3d8b27cbf0d3b604453b00a32b7275
mohamed ghayth afli <mohamed.ghayth12@gmail.com>
2022-05-06 13:59:49 +0000

    Update show.blade.php

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -28,75 +28,56 @@
                                 <div class="card-body">
                                     <div class="user-avatar-section">
                                         <div class="d-flex align-items-center flex-column">
-                                            <img class="img-fluid rounded mt-3 mb-2" src="../../../app-assets/images/portrait/small/avatar-s-2.jpg" height="110" width="110" alt="User avatar" />
+                                            @if(file_exists($user->avatar))
+                                                <img class="img-fluid rounded mt-3 mb-2" src="{{ asset($user->avatar) }}" height="110" width="110" alt="User avatar" />
+                                            @else
+                                            <img class="img-fluid rounded mt-3 mb-2" src="{{ asset('img/config/nopic.png') }}" height="110" width="110" alt="User avatar" />
+                                            @endif
                                             <div class="user-info text-center">
-                                                <h4>Gertrude Barton</h4>
-                                                <span class="badge bg-light-secondary">Author</span>
+                                                <h4>{{$user->name}}</h4>
+                                                @foreach($roles as $role)
+                                                    @if(in_array($role->id, $roles_ids))
+                                                        <span class="badge bg-light-secondary">{{ $role->name }}</span>
+                                                    @endif
+                                                @endforeach
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="d-flex justify-content-around my-2 pt-75">
-                                        <div class="d-flex align-items-start me-2">
-                                            <span class="badge bg-light-primary p-75 rounded">
-                                                <i data-feather="check" class="font-medium-2"></i>
-                                            </span>
-                                            <div class="ms-75">
-                                                <h4 class="mb-0">1.23k</h4>
-                                                <small>Tasks Done</small>
-                                            </div>
-                                        </div>
-                                        <div class="d-flex align-items-start">
-                                            <span class="badge bg-light-primary p-75 rounded">
-                                                <i data-feather="briefcase" class="font-medium-2"></i>
-                                            </span>
-                                            <div class="ms-75">
-                                                <h4 class="mb-0">568</h4>
-                                                <small>Projects Done</small>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <h4 class="fw-bolder border-bottom pb-50 mb-1">Details</h4>
+                                    <h4 class="fw-bolder border-bottom pb-50 mb-1">Détails</h4>
                                     <div class="info-container">
                                         <ul class="list-unstyled">
                                             <li class="mb-75">
-                                                <span class="fw-bolder me-25">Username:</span>
-                                                <span>violet.dev</span>
+                                                <span class="fw-bolder me-25">Email:</span>
+                                                <span>{{$user->email}}</span>
                                             </li>
                                             <li class="mb-75">
-                                                <span class="fw-bolder me-25">Billing Email:</span>
-                                                <span>vafgot@vultukir.org</span>
+                                                <span class="fw-bolder me-25">Statut :</span>
+                                                @if($user->active == true)
+                                                    <span data-v-32017d0f="" class="badge text-capitalize badge-light-success badge-pill"> online </span>
+                                                @else
+                                                    <span data-v-32017d0f="" class="badge text-capitalize badge-light-secondary badge-pill"> offline </span>
+                                                @endif
                                             </li>
                                             <li class="mb-75">
-                                                <span class="fw-bolder me-25">Status:</span>
-                                                <span class="badge bg-light-success">Active</span>
+                                                <span class="fw-bolder me-25">Téléphone :</span>
+                                                <span>{{$user->profil->tel}}</span>
                                             </li>
                                             <li class="mb-75">
-                                                <span class="fw-bolder me-25">Role:</span>
-                                                <span>Author</span>
+                                                <span class="fw-bolder me-25">État :</span>
+                                                <span>{{$user->profil->etat}}</span>
                                             </li>
                                             <li class="mb-75">
-                                                <span class="fw-bolder me-25">Tax ID:</span>
-                                                <span>Tax-8965</span>
-                                            </li>
-                                            <li class="mb-75">
-                                                <span class="fw-bolder me-25">Contact:</span>
-                                                <span>+1 (609) 933-44-22</span>
-                                            </li>
-                                            <li class="mb-75">
-                                                <span class="fw-bolder me-25">Language:</span>
-                                                <span>English</span>
-                                            </li>
-                                            <li class="mb-75">
-                                                <span class="fw-bolder me-25">Country:</span>
-                                                <span>Wake Island</span>
+                                                <span class="fw-bolder me-25">Address :</span>
+                                                <span>{{$user->profil->adresse}}</span>
                                             </li>
                                         </ul>
-                                        <div class="d-flex justify-content-center pt-2">
-                                            <a href="javascript:;" class="btn btn-primary me-1" data-bs-target="#editUser" data-bs-toggle="modal">
-                                                Edit
-                                            </a>
-                                            <a href="javascript:;" class="btn btn-outline-danger suspend-user">Suspended</a>
-                                        </div>
+                                        @if ($user->id != 1 and $user->id !=  Auth::user()->id)
+                                            @if (Auth::user()->can('create-user', 'edit-user', 'destroy-user'))
+                                                <div class="d-flex justify-content-center pt-2">
+                                                    <a href="{{ route('user.destroy', $user->id) }}" class="btn btn-outline-danger suspend-user">Supprimer</a>
+                                                </div>
+                                            @endif
+                                        @endif
                                     </div>
                                 </div>
                             </div>
@@ -108,7 +89,7 @@
                         <div class="col-xl-8 col-lg-7 col-md-7 order-0 order-md-1">
                             <!-- Activity Timeline -->
                             <div class="card">
-                                    <h4 class="card-header">User Activity Timeline</h4>
+                                    <h4 class="card-header">Rapports</h4>
                                     <section class="app-user-list">
                                         <!-- list and filter start -->
 
@@ -118,26 +99,20 @@
                                                     <table id="example" class="display user-list-table table" style="width: 100%;">
                                                         <thead class="table-light">
                                                             <tr>
-                                                                <th>Utilisateur</th>
                                                                 <th>Activité</th>
                                                                 <th>Horodatage</th>
                                                             </tr>
                                                         </thead>
                                                         <tbody>
-                                                            <tr>
-                                                                <td>
-                                                                    <span class="badge bg-secondary">
-                                                                        <i data-feather="star" class="me-25"></i>
-                                                                        <span></span>
-                                                                    </span>
-                                                                </td>
-                                                                <td></td>
-                                                                <td><span class="badge rounded-pill bg-primary"></span></td>
-                                                            </tr>
+                                                            @foreach($user->rapports as $rapport)
+                                                                <tr>
+                                                                    <td>{{$rapport->contenu}}</td>
+                                                                    <td><span class="badge rounded-pill bg-primary">{{$rapport->created_at}}</span></td>
+                                                                </tr>
+                                                            @endforeach
                                                         </tbody>
                                                         <tfoot>
                                                             <tr>
-                                                                <th>Utilisateur</th>
                                                                 <th>Activité</th>
                                                                 <th>Horodatage</th>
                                                             </tr>
